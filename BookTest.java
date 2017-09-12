1fecacb7425e76b067127b3da9fec1c7f0be9f1d
LARumaguera <31608164+LARumaguera@users.noreply.github.com>
2017-09-12 22:30:33 +0800

    Update BookTest.java

--- BookTest.java
+++ BookTest.java
@@ -25,4 +25,27 @@ public class BookTest extends TestCase {
     
     assertEquals(true, secrets.sameAuthor(stone));
   }
+  
+  public void testPublishedBefore() {
+    
+    Author rowling = new Author("J.K. Rowling", "F");
+    Author yoon = new Author("Nicola Yoon", "F");
+    Author mafi = new Author("Tahereh Mafi", "F");
+    Author tolkien = new Author("JRR Tolkien", "M");
+    Author riggs = new Author("Ransom Riggs", "M");
+    
+    Book stone = new Book(rowling, "The Philosophers's Stone", 1997);
+    Book secrets = new Book(rowling, "The Chamber of Secrets", 1998);
+    Book sun = new Book(yoon, "The Sun is Also a Star", 2005);
+    Book ignite = new Book(mafi, "Ignite Me", 2005);
+    Book hobbit = new Book(tolkien, "The Hobbit", 2001);
+    Book peculiar = new Book(riggs, "Miss Peregrine's Home for Peculiar Children", 2003);
+    
+    assertEquals(true, secrets.publishedBefore(stone));
+    assertEquals(false, sun.publishedBefore(ignite));
+    assertEquals(false, hobbit.publishedBefore(peculiar));
+    assertEquals(true, sun.publishedBefore(peculiar));
+    assertEquals(false, hobbit.publishedBefore(ignite));
+    
+  }
 }
